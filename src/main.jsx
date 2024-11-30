55974c22bd15a41fe9717e3b31387e1fae52cd76
Oludire Damilare <oludire@gmail.com>
2024-11-30 20:32:42 +0100

    ✅set up state slices

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-import './store';
+// import './store-v1';
+import store from './store';
+
+store.dispatch({
+  type: 'customer/createCustomer',
+  payload: { fullName: 'Jesse Mosh', nationalID: 'a2jz', createdAt: new Date() }
+});
+
+console.log(store.getState());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
