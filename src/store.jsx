52aa326e5984121d8300c5c8ba9e4021443bee45
Oludire Damilare <oludire@gmail.com>
2025-01-02 06:55:18 +0100

    Update store.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import accountReducer from './features/accounts/accountSlice';
-import customerReducer from './features/customers/customerSlice-v1';
+import customerReducer from './features/customers/customerSlice';
 
 const store = configureStore({
   reducer: {
